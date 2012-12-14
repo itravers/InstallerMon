60c5858c5d4b8018b68ebc4ff1df3201a44dc5cb
Isaac Travers <itravers@digitalpath.net>
2012-12-13 19:10:47 -0800

    Added Navigation for main menu, survey activity and options menu.

--- src/com/digitalpath/installermon/OptionsActivity.java
+++ src/com/digitalpath/installermon/OptionsActivity.java
@@ -2,29 +2,21 @@ package com.digitalpath.installermon;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class OptionsActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_main);
-		showSplashScreen();
+		setContentView(R.layout.activity_options);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
+		getMenuInflater().inflate(R.menu.activity_options, menu);
 		return true;
 	}
-	
-	public void showSplashScreen(){
-		Intent intent = new Intent(this, SplashScreenActivity.class);
-		startActivity(intent);
-	}
-	
 
 }
