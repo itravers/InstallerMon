ac4a70583183d69a90b59020bc03b9c11f20b60b
Isaac Assegai <confusedvirtuoso@gmail.com>
2012-12-14 09:58:22 -0800

    Added Slide in and out right feature.

--- gen/com/digitalpath/installermon/R.java
+++ gen/com/digitalpath/installermon/R.java
@@ -8,26 +8,66 @@
 package com.digitalpath.installermon;
 
 public final class R {
+    public static final class anim {
+        public static final int appear=0x7f040000;
+        public static final int disappear=0x7f040001;
+        public static final int slide_right_enter=0x7f040002;
+        public static final int slide_right_exit=0x7f040003;
+    }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int dplogo=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int SplashImageView=0x7f080009;
+        public static final int TheSplashLayout=0x7f080008;
+        public static final int activity_menu_header=0x7f080001;
+        public static final int activity_menu_imageView1=0x7f080002;
+        public static final int activity_menu_options_button=0x7f080004;
+        public static final int activity_menu_relativeLayout1=0x7f080000;
+        public static final int activity_menu_survey_button=0x7f080003;
+        public static final int activity_survey_header=0x7f08000b;
+        public static final int activity_survey_imageView1=0x7f08000c;
+        public static final int activity_survey_relativeLayout1=0x7f08000a;
+        public static final int imageView1=0x7f080007;
+        public static final int menu_settings=0x7f08000d;
+        public static final int options_activity_header=0x7f080006;
+        public static final int relativeLayout1=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_menu=0x7f030001;
+        public static final int activity_options=0x7f030002;
+        public static final int activity_splash_screen=0x7f030003;
+        public static final int activity_survey=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_activity_header_text=0x7f050008;
+        public static final int menu_settings=0x7f050002;
+        public static final int options_activity_header_text=0x7f05000b;
+        public static final int options_button_name=0x7f050005;
+        public static final int site_survey=0x7f050009;
+        public static final int survey_activity_header_text=0x7f05000c;
+        public static final int survey_button_name=0x7f050006;
+        public static final int title_activity_menu=0x7f050004;
+        public static final int title_activity_options=0x7f05000a;
+        public static final int title_activity_splash_screen=0x7f050003;
+        public static final int title_activity_survey=0x7f050007;
     }
     public static final class style {
+        public static final int Animations=0x7f060002;
+        public static final int Animations_FadeIn=0x7f060006;
+        public static final int Animations_FadeInOut=0x7f060003;
+        public static final int Animations_FadeOut=0x7f060007;
+        public static final int Animations_SlideRightEnterExit=0x7f060004;
+        public static final int Animations_SlideRightExit=0x7f060005;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -49,10 +89,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
+        public static final int Theme_GenericMenu=0x7f060009;
+        public static final int Theme_MainMenu=0x7f060008;
+        public static final int Theme_Transparent=0x7f06000a;
     }
 }
--- src/com/digitalpath/installermon/SurveyActivity.java
+++ src/com/digitalpath/installermon/SurveyActivity.java
@@ -19,7 +19,7 @@ public class SurveyActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_survey, menu);
+		//getMenuInflater().inflate(R.menu.activity_survey, menu);
 		return true;
 	}
 
